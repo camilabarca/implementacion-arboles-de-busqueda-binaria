de80722414041d14f9fa2811d65ec3062eb2dd62
Camila <camila.labarca@ug.uchile.cl>
2021-11-08 12:13:24 -0300

    mas problemas

--- arb.c
+++ arb.c
@@ -48,7 +48,7 @@ BTree* createTree(int B, int l){
 void splitChild(int i, BTree *y, BTree **x){
 
     // Creo nuevo nodo
-    BTree *z = createTree(y->B, y->leaf);
+    BTree *z = createTree(y->B, y->leaf);  
     z->n = (((*x)->B + 1) / 2) - 1;
  
     // Copio ultimos B/2 elem
@@ -65,7 +65,7 @@ void splitChild(int i, BTree *y, BTree **x){
     // Reduce the number of keys in y
     y->n = (((*x)->B + 1) / 2)- 1;
  
-    // Since this node is going to have a new child,
+    // Since this node is going to have a new child,  
     // create space of new child
     for (int j = (*x)->n; j >= i+1; j--)
         (*x)->C[j+1] = (*x)->C[j];
@@ -113,15 +113,68 @@ void splitChild2(int i, BTree *y, BTree **x, int k){
             (*x)->keys[j+1] = (*x)->keys[j];
     
         // Copy the middle key of y to this node
-        (*x)->keys[i] = k;
+        (*x)->keys[i] = y->keys[(((*x)->B + 1) / 2)-1];
     
         // Increment count of keys in this node
         (*x)->n = (*x)->n + 1;
     } else if (k > y->keys[pos_mediana-1]){
+        z->n = (*x)->B/2-1;
+        for (int j = 0; j < (*x)->B/2-1; j++)
+            z->keys[j] = y->keys[j+(*x)->B/2+1];
 
+        if (y->leaf == 0)
+        {
+            for (int j = 0; j < (*x)->B/2; j++)
+                z->C[j] = y->C[j+(*x)->B/2+1];
+        }
+        y->n = (*x)->B/2;
+        for (int j = (*x)->n; j >= i+1; j--)
+            (*x)->C[j+1] = (*x)->C[j];
+        // Link the new child to this node
+        (*x)->C[i+1] = z;
+        // A key of y will move to this node. Find the location of
+        // new key and move all greater keys one spa ce ahead
+        for (int j = (*x)->n-1; j >= i; j--)
+            (*x)->keys[j+1] = (*x)->keys[j];
+    
+        // Copy the middle key of y to this node
+        (*x)->keys[i] = y->keys[(*x)->B/2];
+    
+        // Increment count of keys in this node
+        (*x)->n = (*x)->n + 1;
     } else {
-        splitChild(i, y, x);
-    } 
+        z->n = (*x)->B/2;
+        for (int j = 0; j < (*x)->B/2; j++)
+            z->keys[j] = y->keys[j+(*x)->B/2];
+
+        if (y->leaf == 0)
+        {
+            for (int j = 0; j < (*x)->B/2+1; j++)
+                z->C[j] = y->C[j+(*x)->B/2];
+        }
+        // Reduce the number of keys in y
+        y->n = (((*x)->B + 1) / 2)- 1;
+    
+        // Since this node is going to have a new child,  
+        // create space of new child
+        for (int j = (*x)->n; j >= i+1; j--)
+            (*x)->C[j+1] = (*x)->C[j];
+    
+        // Link the new child to this node
+        (*x)->C[i+1] = z;
+    
+        // A key of y will move to this node. Find the location of
+        // new key and move all greater keys one space ahead
+        for (int j = (*x)->n-1; j >= i; j--)
+            (*x)->keys[j+1] = (*x)->keys[j];
+    
+        // Copy the middle key of y to this node
+        (*x)->keys[i] = y->keys[(((*x)->B + 1) / 2)-1];
+    
+        // Increment count of keys in this node
+        (*x)->n = (*x)->n + 1;
+
+    }
 }
 
 
@@ -158,15 +211,29 @@ void insertNonFull(int k, BTree **b)
         if ((a->C[i+1])->n == a->B)
         {
             // If the child is full, then split it
-            splitChild(i+1, a->C[i+1], &a);
- 
-            // After split, the middle key of C[i] goes up and
-            // C[i] is splitted into two.  See which of the two
-            // is going to have the new key
-            if (a->keys[i+1] < k)
-                i++;
+            if (a->B % 2 != 0){
+                splitChild(i+1, a->C[i+1], &a);
+                // After split, the middle key of C[i] goes up and
+                // C[i] is splitted into two.  See which of the two
+                // is going to have the new key
+                if (a->keys[i+1] < k)
+                    i++;
+                insertNonFull(k, &(a->C[i+1]));
+            } else {
+                if (k > a->keys[a->B/2-1] && k < a->keys[a->B/2]){
+                    splitChild2(i+1, a->C[i+1], &a, k);
+                } else {
+                    splitChild2(i+1, a->C[i+1], &a, k);
+                    if (a->keys[i+1] < k)
+                        i++;
+                    insertNonFull(k, &(a->C[i+1]));
+
+                }
+                
+            }
+            
         }
-        insertNonFull(k, &(a->C[i+1]));
+        
         *b = a;
     }
 }
@@ -197,7 +264,7 @@ void insert(int k, BTreeRoot **tree)
             // Make old root as child of new root
             s->C[0] = a->root;
  
-            //if (a->B % 2 != 0){
+            if (a->B % 2 != 0){
                 splitChild(0, a->root, &s);
                 // New root has two children now.  Decide which of the
                 // two children is going to have new key
@@ -206,9 +273,18 @@ void insert(int k, BTreeRoot **tree)
                     i++;
                 insertNonFull(k, &(s->C[i]));
 
-            /*} else {
-                splitChild2(0, a->root, &s, k);
-            }*/
+            } else {
+                if (k > a->root->keys[s->B/2-1] && k < a->root->keys[s->B/2]){
+                    splitChild2(0, a->root, &s, k);
+                } else {
+                    splitChild2(0, a->root, &s, k);
+                    int i = 0;
+                    if (s->keys[0] < k)
+                        i++;
+                    insertNonFull(k, &(s->C[i]));
+                }
+                
+            }
             
 
  
@@ -246,14 +322,15 @@ void traverse(BTree *a)
 int main(){
     BTreeRoot *a = (BTreeRoot*) malloc(sizeof(BTreeRoot*));
     a->root = NULL;
-    a->B = 4;
+    a->B = 6;
     insert(3, &a);
     insert(2, &a);
     insert(5, &a);
     insert(6, &a);
+    insert(7, &a);
+    insert(8, &a);
     insert(1, &a);
     traverse(a->root);
     return 1;
 }
-
 
