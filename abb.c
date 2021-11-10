cc4b10ff3fcd731ad8c94dcd7a1b7307de19bf4e
Konrad Ivelic <konrad.ivelic18@gmail.com>
2021-11-10 19:13:11 -0300

    ABBCorrible

--- abb.c
+++ abb.c
@@ -6,12 +6,12 @@
 #include <time.h>
 #include <math.h>
 
-#define n 100 //10^6
+#define n 1000000 //10^6
 #define pi 0.5
 #define pbe 1/3
 #define pbi 1/6
-#define tamPaq 10
-#define nSec 2
+#define tamPaq 1000
+#define nSec 100
 
 // Tiempos
 float t[n/tamPaq] = {0};
@@ -82,9 +82,9 @@ int random_number(int min_num, int max_num)
         if (min_num < max_num)
         {
             low_num = min_num;
-            hi_num = max_num + 1; // include max_num in output
+            hi_num = max_num + 1; // incluye max_num en output
         } else {
-            low_num = max_num + 1; // include max_num in output
+            low_num = max_num + 1; // incluye max_num en output
             hi_num = min_num;
         }
         srand(time(NULL));
@@ -92,26 +92,21 @@ int random_number(int min_num, int max_num)
         return result;
     }
 
-//encontrar techo de r en arr
+//Encontrar techo de r en arr
 int findCeil(int arr[], int r, int l, int h)
 {
     int mid;
     while (l < h)
     {
-         mid = l + ((h - l) >> 1);  // Same as mid = (l+h)/2
+         mid = l + ((h - l) >> 1); 
         (r > arr[mid]) ? (l = mid + 1) : (h = mid);
     }
     return (arr[l] >= r) ? l : -1;
 }
 
-// The main function that returns a random number from arr[] according to
-// distribution array defined by freq[]. n is size of arrays.
+// Funcion que retorna un numero aleatorio de arr[] segun parametros
 int myRand(int arr[], int freq[], int size, int prefix[]){
-    // prefix[n-1] is sum of all frequencies. Generate a random number
-    // with value from 1 to this sum
     int r = (rand() % prefix[size - 1]) + 1;
- 
-    // Find index of ceiling of r in prefix array
     int indexc = findCeil(prefix, r, 0, size - 1);
     return arr[indexc];
 }
@@ -120,12 +115,12 @@ int* generarSecAleatoria(int *s){
     int nums[n] = {0};
     int in_nums[1000000] = {0};
     int num = random_number(1, 1000000);
-    int *secAl = malloc(sizeof(int)*(n+1)); // Agrego el numero inicial a insertar -1 +1
+    int *secAl = malloc(sizeof(int)*(n+1)); 
 
     int size_array = 1;
     nums[0] = num;
     in_nums[num-1] = 1;
-    secAl[0] = num;
+    secAl[0] = num; // Agrego el numero inicial a insertar
     
     for (int j = 0; j< n-1;j++){
         if (s[j] == 0){ // Inserción
@@ -155,6 +150,97 @@ int* generarSecAleatoria(int *s){
     return secAl;
 }
 
+int* generarSecCreciente(int *s, float k){
+    int nums[n] = {0};
+    int in_nums[1000000] = {0};
+    int num = 0;
+    int *secCre = malloc(sizeof(int)*(n+1)); 
+
+    int m = 1;
+    nums[0] = num;
+    in_nums[num] = 1;
+    secCre[0] = num; // Agrego el numero inicial a insertar
+    for (int j = 0; j< n-1;j++){
+        if (s[j] == 0){
+            int num = random_number(0, k*m);
+            num += m;
+            while (in_nums[num] == 1){
+                num = random_number(0, k*m);
+                num += m;
+            }
+
+            nums[m] = num;
+            secCre[j+1] = num;
+            in_nums[num] = 1;
+            m++;
+        } else if (s[j] == 1){
+            int ind = rand()%m;
+            int num = nums[ind];
+            secCre[j+1] = num; 
+        } else {
+            int num = random_number(0, 1000000);
+            while (in_nums[num] == 1){
+                num = random_number(0, 1000000);
+            }
+            secCre[j+1] = num;         
+        }
+    }
+    return secCre;
+}
+
+int* generarSecSesgada(int *s, int px){
+    int nums[n] = {0};
+    int in_nums[1000000] = {0};
+    int *secSes = malloc(sizeof(int)*(n+1));
+    int peso[n];
+    int prefix[n];
+    int num = random_number(1, 1000000);
+    
+    int size_array = 1;
+    nums[0] = num;
+    secSes[0] = num;
+    if (px == 0){
+        peso[0] = num;
+    } else if (px == 1){
+        peso[0] = (int) sqrt((float)num);
+    } else {
+        peso[0] = (int) log((float)num);
+    }
+    prefix[0] = peso[0];
+    in_nums[num-1] = 1;
+    for (int j = 0; j< n-1;j++){
+        if (s[j] == 0){
+            int num = random_number(1, 1000000);
+            while (in_nums[num-1] == 1){
+                num = random_number(1, 1000000);
+            }
+            nums[size_array] = num;
+            secSes[j+1] = num;
+            if (px == 0){
+                peso[size_array] = num;
+            } else if (px == 1){
+                peso[size_array] =  (int) sqrt((float)num);
+            } else {
+                peso[size_array] = (int) log((float)num);
+            }
+            prefix[size_array] = prefix[size_array-1] + peso[size_array];
+            in_nums[num-1] = 1;
+            size_array++;
+        } else if (s[j] == 1){
+            int num = myRand(nums, peso, size_array, prefix);
+            secSes[j+1] = num;
+
+        } else {
+            int num = random_number(1, 1000000);
+            while (in_nums[num-1] == 1){
+                num = random_number(1, 1000000);
+            }
+            secSes[j+1] = num;
+        }
+    }
+    return secSes;
+}
+
 void correrSecuencia(int *op, int *sec, ABB *tree){
     // Medicion de tiempo
     int i = 0; // Recorre los indices de t
@@ -178,211 +264,134 @@ void correrSecuencia(int *op, int *sec, ABB *tree){
             //fprintf(stderr,"%f\n",(float) start_t);
             //fprintf(stderr,"%f\n",(float) end_t);
             float total_t = ((float)(end_t - start_t)) / CLOCKS_PER_SEC;
-            t[i] = t[i] + total_t/tamPaq; // Tiempo promedio de las 1000 operaciones
+            //fprintf(stderr,"%f\n",t[i]);
+            t[i] += total_t/tamPaq; // Tiempo promedio de las 1000 operaciones
             start_t = end_t; // Anoto como tiempo inicial el actual
-            fprintf(stderr,"Tiempo de paquete %d es: %f\n", (i+1),t[i]);
+            fprintf(stderr,"Tiempo de paquete %d es: %f\n", (i+1),total_t/tamPaq);
             i ++;
         }
     }
 }
-
-/*
-void secAleatoria(int *s, ABB *tree, float *t){
-    int nums[n] = {0};
-    int in_nums[1000000] = {0};
-    int num = random_number(1, 1000000);
-    // Medicion de tiempo
-    int i = 0; // Recorre los indices de t
-    clock_t start_t, end_t, total_t;
-    start_t = clock(); // Tiempo inicial, arrancar el reloj
-    fprintf(stderr,"%f\n",(float) start_t);
-    insert(num, &tree);
-    //fprintf(stderr, "Se inserto %d\n", num);
-    int size_array = 1;
-    nums[0] = num;
-    in_nums[num-1] = 1;
-
-    for (int j = 0; j< n-1;j++){
-        if (s[j] == 0){ // Inserción
-            int num = random_number(1, 1000000);
-            while (in_nums[num-1] == 1){
-                num = random_number(1, 1000000);
-            }
-            insert(num, &tree);
-            //fprintf(stderr, "Se inserto %d\n", num);
-            nums[size_array] = num;
-            in_nums[num-1] = 1;
-            size_array++;
-        } else if (s[j] == 1){ // Busqueda exitosa
-            int ind = rand()%size_array;
-            int num = nums[ind];
-
-            //fprintf(stderr, "Se busco exitosamente %d y el resultado fue %d\n", num, find(num, &tree));
-        } else { // Busqueda infructuosa
-            int num = random_number(1, 1000000);
-            while (in_nums[num-1] == 1){
-                num = random_number(1, 1000000);
-            }
-            //fprintf(stderr, "Se busco infructuasamente %d y el resultado fue %d\n", num, find(num, &tree));
-        }
-        if ((j+2)%10 == 0){ //Pasaron 1000 operaciones y debo registrar tiempos
-            end_t = clock(); // Tiempo actual
-            fprintf(stderr,"%f\n",(float) end_t);
-            total_t = ((float)(end_t - start_t)) / CLOCKS_PER_SEC;
-            t[i] += total_t/(n/tamPaq); // Tiempo promedio de las 1000 operaciones
-            start_t = end_t; // Anoto como tiempo inicial el actual
-            fprintf(stderr,"Tiempo de paquete %d es: %f\n", (i+1),t[i]);
-            i ++;
-        }
-    }
-}
-*/
-
-void secCreciente(int *s, ABB *tree, float k){
-    int nums[n] = {0};
-    int in_nums[1000000] = {0};
-    int num = 0;
-    insert(num, &tree); //Inserto 0 de inicio
-    fprintf(stderr, "Se inserto %d\n", num);
-    int m = 1;
-    nums[0] = num;
-    in_nums[num] = 1;
-    for (int j = 0; j< n-1;j++){
-        if (s[j] == 0){
-            int num = random_number(0, k*m);
-            num += m;
-            while (in_nums[num] == 1){
-                num = random_number(0, k*m);
-                num += m;
-            }
-            insert(num, &tree);
-            fprintf(stderr, "Se inserto %d\n", num);
-            nums[m] = num;
-            in_nums[num] = 1;
-            m++;
-        } else if (s[j] == 1){
-            int ind = rand()%m;
-            int num = nums[ind];
-
-            fprintf(stderr, "Se busco exitosamente %d y el resultado fue %d\n", num, find(num, &tree));
-        } else {
-            int num = random_number(0, 1000000);
-            while (in_nums[num] == 1){
-                num = random_number(0, 1000000);
-            }
-            fprintf(stderr, "Se busco infructuasamente %d y el resultado fue %d\n", num, find(num, &tree));
-        }
-    }
-}
-
-void secSesgada(int *s, ABB *tree, int px){
-    int nums[n] = {0};
-    int in_nums[1000000] = {0};
-    int peso[n];
-    int prefix[n];
-    int num = random_number(1, 1000000);
-    insert(num, &tree);
-    fprintf(stderr, "Se inserto %d\n", num);
-    int size_array = 1;
-    nums[0] = num;
-    if (px == 0){
-        peso[0] = num;
-    } else if (px == 1){
-        peso[0] = (int) sqrt((float)num);
-    } else {
-        peso[0] = (int) log((float)num);
-    }
-    prefix[0] = peso[0];
-    in_nums[num-1] = 1;
-    for (int j = 0; j< n-1;j++){
-        if (s[j] == 0){
-            int num = random_number(1, 1000000);
-            while (in_nums[num-1] == 1){
-                num = random_number(1, 1000000);
-            }
-            insert(num, &tree);
-            fprintf(stderr, "Se inserto %d\n", num);
-            nums[size_array] = num;
-            if (px == 0){
-                peso[size_array] = num;
-            } else if (px == 1){
-                peso[0] =  (int) sqrt((float)num);
-            } else {
-                peso[0] = (int) log((float)num);
-            }
-            prefix[size_array] = prefix[size_array-1] + peso[size_array];
-            in_nums[num-1] = 1;
-            size_array++;
-        } else if (s[j] == 1){
-            int num = myRand(nums, peso, size_array, prefix);
-
-            fprintf(stderr, "Se busco exitosamente %d y el resultado fue %d\n", num, find(num, &tree));
-        } else {
-            int num = random_number(1, 1000000);
-            while (in_nums[num-1] == 1){
-                num = random_number(1, 1000000);
-            }
-            fprintf(stderr, "Se busco infructuasamente %d y el resultado fue %d\n", num, find(num, &tree));
-        }
-    }
-}
-
-
 
 int main(){
+    // Archivo a guardar 
+    FILE * fp;
+
+    fp = fopen ("abb.csv", "w+");
+
     // Secuencia de operaciones
     int *s = secuenciaOper();
     ABB *tree = NULL;
-
-    //int *secAl = generarSecAleatoria(s);
-    //correrSecuencia(s,secAl,tree);
-
-    ///*
+    
+    
     //Secuencia aleatoria
     for (int j = 0; j < n/tamPaq; j++){ // Reinicio tiempos a 0
         t[j] = 0;
     }
     int *secAl;
-    //float t[tamPaq] = {0}; //1000 tiempos, calculados cada 1000 operaciones, total de 10^6 operaciones
     for (int i = 0; i < nSec; i++){
         secAl = generarSecAleatoria(s);
         correrSecuencia(s,secAl,tree); // Calculo tiempos de la secuencia i
-        fprintf(stderr, "Se termino la secuencia %d!\n",i+1);
+        fprintf(stderr, "Se termino la secuencia aleatoria %d!\n",i+1);
         tree = NULL;
     }
     for (int j = 0; j < n/tamPaq ; j++){  
-        printf("%f\n",t[j]);
         t[j] = t[j]/nSec; // Divido por totales de secuencias
-        printf("%f\n",t[j]);
-        printf("\n");
+        fprintf(fp, "%f,", t[j]); // Anoto los tiempos promedios de cada paquete en el archivo
     }
-    //*/
-   
-    /*
-    //Secuencia creciente
-    printf("\n");
-    secCreciente(s,tree, 0.1);
-    tree = NULL;
+    fprintf(fp, "\n");
 
+    //Secuencia creciente k = 0.1
     printf("\n");
-    secCreciente(s,tree, 0.5);
-    tree = NULL;
+    for (int j = 0; j < n/tamPaq; j++){ // Reinicio tiempos a 0
+        t[j] = 0;
+    }
+    int *secCre1;
+        for (int i = 0; i < nSec; i++){
+        secCre1 = generarSecCreciente(s, 0.1);
+        correrSecuencia(s,secCre1,tree); // Calculo tiempos de la secuencia i
+        fprintf(stderr, "Se termino la secuencia creciente %d!\n",i+1);
+        tree = NULL;
+    }
+    for (int j = 0; j < n/tamPaq ; j++){  
+        t[j] = t[j]/nSec; // Divido por totales de secuencias
+        fprintf(fp, "%f,", t[j]); // Anoto los tiempos promedios de cada paquete en el archivo
+    }
+    fprintf(fp, "\n");
+
+    //Secuencia creciente k = 0.5
+    printf("\n");
+    for (int j = 0; j < n/tamPaq; j++){ // Reinicio tiempos a 0
+        t[j] = 0;
+    }
+    int *secCre5;
+        for (int i = 0; i < nSec; i++){
+        secCre5 = generarSecCreciente(s, 0.5);
+        correrSecuencia(s,secCre5,tree); // Calculo tiempos de la secuencia i
+        fprintf(stderr, "Se termino la secuencia creciente %d!\n",i+1);
+        tree = NULL;
+    }
+    for (int j = 0; j < n/tamPaq ; j++){  
+        t[j] = t[j]/nSec; // Divido por totales de secuencias
+        fprintf(fp, "%f,", t[j]); // Anoto los tiempos promedios de cada paquete en el archivo
+    }
+    fprintf(fp, "\n");
 
     //Secuencia sesgada con p(x) = x
     printf("\n");
-    secSesgada(s,tree, 0);
-    tree = NULL;
+    for (int j = 0; j < n/tamPaq; j++){ // Reinicio tiempos a 0
+        t[j] = 0;
+    }
+    int *sesSesX;
+        for (int i = 0; i < nSec; i++){
+        sesSesX = generarSecSesgada(s, 0);
+        correrSecuencia(s,sesSesX,tree); // Calculo tiempos de la secuencia i
+        fprintf(stderr, "Se termino la secuencia sesgada p(x)=x %d!\n",i+1);
+        tree = NULL;
+    }
+    for (int j = 0; j < n/tamPaq ; j++){  
+        t[j] = t[j]/nSec; // Divido por totales de secuencias
+        fprintf(fp, "%f,", t[j]); // Anoto los tiempos promedios de cada paquete en el archivo
+    }
+    fprintf(fp, "\n");
 
     //Secuencia sesgada con p(x) = sqrt(x)
     printf("\n");
-    secSesgada(s,tree, 1);
-    tree = NULL;
+    for (int j = 0; j < n/tamPaq; j++){ // Reinicio tiempos a 0
+        t[j] = 0;
+    }
+    int *sesSesSQ;
+        for (int i = 0; i < nSec; i++){
+        sesSesSQ = generarSecSesgada(s, 1);
+        correrSecuencia(s,sesSesSQ,tree); // Calculo tiempos de la secuencia i
+        fprintf(stderr, "Se termino la secuencia sesgada p(x)=sqrt(x) %d!\n",i+1);
+        tree = NULL;
+    }
+    for (int j = 0; j < n/tamPaq ; j++){  
+        t[j] = t[j]/nSec; // Divido por totales de secuencias
+        fprintf(fp, "%f,", t[j]); // Anoto los tiempos promedios de cada paquete en el archivo
+    }
+    fprintf(fp, "\n");
 
     //Secuencia sesgada con p(x) = ln(x)
     printf("\n");
-    secSesgada(s,tree, 2);
-    tree = NULL;
-    */
+    for (int j = 0; j < n/tamPaq; j++){ // Reinicio tiempos a 0
+        t[j] = 0;
+    }
+    int *sesSesLN;
+        for (int i = 0; i < nSec; i++){
+        sesSesLN = generarSecSesgada(s, 2);
+        correrSecuencia(s,sesSesLN,tree); // Calculo tiempos de la secuencia i
+        fprintf(stderr, "Se termino la secuencia sesgada p(x)=ln(x) %d!\n",i+1);
+        tree = NULL;
+    }
+    for (int j = 0; j < n/tamPaq ; j++){  
+        t[j] = t[j]/nSec; // Divido por totales de secuencias
+        fprintf(fp, "%f,", t[j]); // Anoto los tiempos promedios de cada paquete en el archivo
+    }
+    fprintf(fp, "\n");
+
+    fclose(fp);
 
     return 1;
 }
